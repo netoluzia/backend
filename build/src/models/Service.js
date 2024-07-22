0fea0aee48b0ad4c133cc04325b43c29110fc7f3
honorio-sousa-neto <honoriossneto@gmail.com>
2024-07-22 15:11:27 +0100

    amen

--- build/src/models/Service.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.servicesType = void 0;
-var servicesType;
-(function (servicesType) {
-    servicesType["SERVICE"] = "service";
-    servicesType["PRODUCT"] = "product";
-})(servicesType || (exports.servicesType = servicesType = {}));
